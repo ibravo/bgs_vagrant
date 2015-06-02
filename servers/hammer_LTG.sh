ccdfe6b8d9c7b103676eb1e4a616ec7d65737536
Ignacio Bravo <ibravo@hotmail.com>
2015-06-02 11:38:18 -0400

    Added line for 2.Partition table

--- servers/hammer_LTG.sh
+++ servers/hammer_LTG.sh
@@ -4,6 +4,9 @@ echo "1. Create LTG CentOS"
 hammer -u admin -p octopus medium create --name "CentOS LTG" --os-family Redhat --path http://katello.hq.ltg/pulp/repos/LTG_Federal/Library/custom/CentOS7/Centos_7_x86_64
 
 echo "2. Create Partition Table"
+hammer -u admin -p octopus --help
+
+
 echo "3. Modify EPEL repo on Provisioning template"
 echo "4. Modify PXE Provisioning to allow for cciss drivers"
 
